8dd88de2097c4065d0dad4006f2afd43318a84b2
I-Lun <b06902135@ntu.edu.tw>
2023-02-28 15:50:22 -0800

    ok

--- src/student_code.cpp
+++ src/student_code.cpp
@@ -76,17 +76,16 @@ namespace CGL
     // taking the area-weighted average of the normals of neighboring
     // triangles, then normalizing.
 
-    // TODO: Change the do while loop into normal while loop
-    // TODO: Check for boundary faces within do-while loop?
     Vector3D area_weighted_normal(0,0,0);
     HalfedgeCIter half_edge = halfedge();
-    do {
+    while (1) {
       Vector3D vertex_0 = position;
       Vector3D vertex_1 = half_edge -> next() -> vertex() -> position;
       Vector3D vertex_2 = half_edge -> next() -> next() -> vertex() -> position;
       area_weighted_normal += cross(vertex_1 - vertex_0, vertex_2 - vertex_0);
       half_edge = half_edge -> twin() -> next();
-    } while (half_edge != halfedge());
+      if (half_edge == halfedge())  break;
+    }
     return area_weighted_normal.unit();
   }
 
@@ -99,22 +98,21 @@ namespace CGL
     HalfedgeIter half_edge = e0 -> halfedge(), half_edge_next = half_edge -> next(), half_edge_prev = half_edge_next -> next();
     HalfedgeIter twin = half_edge -> twin(), twin_next = twin -> next(), twin_prev = twin_next -> next();
     
-    // Set upper triangle face
-    // TODO: change order of these updates
+    // Set upper triangle face, counter-clockwisely
     half_edge -> setNeighbors(twin_prev, twin, half_edge_prev -> vertex(), e0, twin_prev -> face());
     twin_prev -> next() = half_edge_next;
     half_edge_next -> next() = half_edge;
-    half_edge_next -> face() = twin_prev -> face();
     twin_prev -> face() -> halfedge() = half_edge;
     half_edge_next -> vertex() -> halfedge() = half_edge_next;
+    half_edge_next -> face() = twin_prev -> face();
 
     // Set lower triangle face
     twin -> setNeighbors(half_edge_prev, half_edge, twin_prev -> vertex(), e0, half_edge_prev -> face());
     half_edge_prev -> next() = twin_next;
     twin_next -> next() = twin;
-    twin_next -> face() = half_edge_prev -> face();
     half_edge_prev -> face() -> halfedge() = twin;
     twin_next -> vertex() -> halfedge() = twin_next;
+    twin_next -> face() = half_edge_prev -> face();
 
     return EdgeIter();
   }
@@ -124,88 +122,19 @@ namespace CGL
     // TODO Part 5.
     // This method should split the given edge and return an iterator to the newly inserted vertex.
     // The halfedge of this vertex should point along the edge that was split, rather than the new edges.
-    // if (e0 -> isBoundary()) return e0 -> halfedge() -> vertex();
 
-    // // Get existing halfedges and faces
-    // HalfedgeIter half_edge = e0 -> halfedge(), half_edge_next = half_edge -> next(), half_edge_prev = half_edge_next -> next();
-    // HalfedgeIter twin = half_edge -> twin(), twin_next = twin -> next(), twin_prev = twin_next -> next();
-    // FaceIter upper_left_face = half_edge -> face(), upper_right_face = twin -> face();
-
-    // // Create new halfedges, edges, and faces
-    // // TODO: Use vectors or maps (to give name to indices)
-    // HalfedgeIter boundary_halfedges[] = {half_edge_next, half_edge_prev, twin_next, twin_prev};
-    // HalfedgeIter outward_halfedges[] = {newHalfedge(), newHalfedge(), newHalfedge(), newHalfedge()};
-    // HalfedgeIter inward_e0, newEdge(), newEdge(), newEdge()};
-    // FaceIter faces[] = {upper_left_face, upper_right_face, newFace(), newFace()};
-
-    // // Create new vertex
-    // VertexIter new_vertex = newVertex();
-    // new_vertex -> position = (half_edge -> vertex() -> position + twin -> vertex() -> position) / 2.0;
-    // new_vertex -> halfedge() = outward_halfedges[0];
-
-    
-    // // Updating variables for each face 
-    // // TODO: unroll for loop
-    // for (int i = 0; i < 4; i++) {
-    //   // TODO: reorder updates
-    //   // Updating new halfedges
-    //   outward_halfedges[i] -> setNeighbors(boundary_halfedges[i], inward_halfedges[(i + 3) % 4], new_vertex, edges[i], faces[i]);
-    //   inward_halfedges[i] -> setNeighbors(outward_halfedges[i], outward_halfedges[(i + 1) % 4], boundary_halfedges[(i + 1) % 4] -> vertex(), edges[(i + 1) % 4], faces[i]);
-    //   boundary_halfedges[i] -> next() = inward_halfedges[i];
-    //   boundary_halfedges[i] -> face() = faces[i];
-
-    //   // Updating new edges and faces
-    //   edges[i] -> halfedge() = outward_halfedges[i];
-    //   faces[i] -> halfedge() = outward_halfedges[i];
-
-    //   // Updating old vertex
-    //   inward_halfedges[i] -> vertex() -> halfedge() = inward_halfedges[i];
-
-    // }
-
-    // // Remove old halfedges
-    // deleteHalfedge(half_edge);
-    // deleteHalfedge(twin);
-
-    
-    // return new_vertex
-
-    // TODO: Change order of declarations
     // view e0 as c_m_c edge
     if (e0 -> isBoundary()) return e0 -> halfedge() -> vertex();
     
+    // --------------- Initialization ------------------
     // Inner Halfedges
+    // upper part of mesh split
     HalfedgeIter m_c = e0 -> halfedge();
     HalfedgeIter c_a = m_c -> next();
     HalfedgeIter a_m = c_a -> next();
     HalfedgeIter c_m = m_c -> twin();
     HalfedgeIter m_d = c_m -> next();
     HalfedgeIter d_c = m_d -> next();
-
-    // Outer Halfedges
-    HalfedgeIter a_c = c_a -> twin();
-    HalfedgeIter b_a = a_m -> twin();
-    HalfedgeIter d_b = m_d -> twin();
-    HalfedgeIter c_d = d_c -> twin();
-
-    // Vertices
-    VertexIter b = m_c -> vertex();  // first vertex at endpoint of previous edge
-    VertexIter c = c_m -> vertex();  // second vertex at opposite endpoint of previous edge
-    VertexIter a = a_m -> vertex();  // first vertex at endpoint of flipped edge
-    VertexIter d = d_c -> vertex();  // second vertex at opposite endpoint of flipped edge
-
-    // Edges
-    EdgeIter a_c_a = c_a -> edge();
-    EdgeIter a_b_a = a_m -> edge();
-    EdgeIter b_d_b = m_d -> edge();
-    EdgeIter c_d_c = d_c -> edge();
-
-    // Faces
-    FaceIter upper_left_face = m_c -> face();       // face 1
-    FaceIter upper_right_face = c_m -> face();       // face 2
-
-    // --------------- m == b before this line ------------
-  
     // New inner halfedges for lower part of mesh split
     HalfedgeIter b_m = newHalfedge();
     HalfedgeIter m_a = newHalfedge();
@@ -214,18 +143,36 @@ namespace CGL
     HalfedgeIter b_d = newHalfedge();
     HalfedgeIter d_m = newHalfedge();
 
+    // Outer Halfedges
+    HalfedgeIter a_c = c_a -> twin();
+    HalfedgeIter b_a = a_m -> twin();
+    HalfedgeIter d_b = m_d -> twin();
+    HalfedgeIter c_d = d_c -> twin();
+
+    // Vertices
+    VertexIter a = a_m -> vertex();  
+    VertexIter b = m_c -> vertex();  
+    VertexIter d = d_c -> vertex();  
+    VertexIter c = c_m -> vertex();  
     // New vertex
     VertexIter m = newVertex();
-    
+
+    // Edges
+    EdgeIter a_c_a = c_a -> edge();
+    EdgeIter a_b_a = a_m -> edge();
+    EdgeIter b_d_b = m_d -> edge();
+    EdgeIter c_d_c = d_c -> edge();
     // New edges
     EdgeIter a_m_a = newEdge();
     EdgeIter b_m_b = newEdge();
     EdgeIter d_m_d = newEdge();
 
+    // Faces
+    FaceIter upper_left_face = m_c -> face();       // face 1
+    FaceIter upper_right_face = c_m -> face();       // face 2
     // New faces
     FaceIter bottom_left_face = newFace();
     FaceIter bottom_right_face = newFace();
-
 
     // --------------- UPDATES ------------------
 
@@ -248,38 +195,38 @@ namespace CGL
     d_m -> setNeighbors(m_b, m_d, d, d_m_d, bottom_right_face);
 
     // Vertices
-    // Update vertex position to edge midpoint
-    m -> position = (b -> position + c -> position) / 2.0;
     // Update vertex status to isNew
     m -> isNew = true;
+    // Update vertex position to edge midpoint
+    m -> position = (a -> position + d -> position) / 2.0;
     // Update vertice halfedges          
-    b -> halfedge() = b_m;
-    c -> halfedge() = c_a;
-    a -> halfedge() = a_b;
-    d -> halfedge() = d_c;
     m -> halfedge() = m_c;
+    a -> halfedge() = a_b;
+    b -> halfedge() = b_m;
+    d -> halfedge() = d_c;
+    c -> halfedge() = c_a;
     
     // Edges
-    e0 -> halfedge() = m_c;
-    a_c_a -> halfedge() = c_a;
     a_b_a -> halfedge() = a_b;
     b_d_b -> halfedge() = b_d;
     c_d_c -> halfedge() = d_c;
+    a_c_a -> halfedge() = c_a;
+    e0 -> halfedge() = m_c; //e0 = c_m_c
     a_m_a -> halfedge() = a_m;
     b_m_b -> halfedge() = b_m;
     d_m_d -> halfedge() = m_d;
     
     // Set isNew to true for edges touching the new vertex 
     e0 -> isNew = false; // half of original edge
-    b_m_b -> isNew = false; // half of original edge
     a_m_a -> isNew = true; // bisecting edge half
+    b_m_b -> isNew = false; // half of original edge
     d_m_d -> isNew = true; // bisecting edge half
 
     // Faces
     upper_left_face -> halfedge() = m_c;
-    upper_right_face -> halfedge() = c_m;
     bottom_left_face -> halfedge() = b_m;
     bottom_right_face -> halfedge() = m_b;
+    upper_right_face -> halfedge() = c_m;
     
     // Return the new vertex
     return m;
@@ -293,87 +240,69 @@ namespace CGL
     // This routine should increase the number of triangles in the mesh using Loop subdivision.
     // One possible solution is to break up the method as listed below.
 
-    // 2. Compute the updated vertex positions associated with edges, and store it in Edge::newPosition.
-    for (EdgeIter e = mesh.edgesBegin(); e != mesh.edgesEnd();e++) {
-            // Set isNew to false for all old edges
-            e -> isNew = false;
-            
-            
-            // Inner Halfedges
-            HalfedgeIter h0 = e -> halfedge();
-            HalfedgeIter h1 = h0 -> next();
-            HalfedgeIter h2 = h1 -> next();
-            HalfedgeIter h3 = h0 -> twin();
-            HalfedgeIter h4 = h3 -> next();
-            HalfedgeIter h5 = h4 -> next();
-
-            // Vertices
-            VertexIter b = h0 -> vertex();  // first vertex at endpoint of previous edge
-            VertexIter a = h3 -> vertex();  // second vertex at opposite endpoint of previous edge
-            VertexIter c = h2 -> vertex();  // first vertex at endpoint of flipped edge
-            VertexIter d = h5 -> vertex();  // second vertex at opposite endpoint of flipped edge
-
-            // Precompute and store position of new vertices in newPosition field of old edges
-            e -> newPosition = 3.0 / 8.0 * (b -> position + a -> position) + 1.0 / 8.0 * (c -> position + d -> position);
-            
-    }
-    
-    
-    // 1. Compute new positions for all the vertices in the input mesh, using the Loop subdivision rule,
-    // and store them in Vertex::newPosition. At this point, we also want to mark each vertex as being
-    // a vertex of the original mesh.
+    // Step A: Compute the positions of both new and old vertices using the original mesh. 
+    // We want to perform these computations before subdivision because traversing a coarse mesh is 
+    // much easier than traversing a subdivided mesh with more elements.
     for (VertexIter v = mesh.verticesBegin(); v != mesh.verticesEnd(); v++) { 
-            // set isNew to false for all old vertices
-            v -> isNew = false;
-            
-            // Set newPosition of old vertices as weighted combination of surrounding old vertices
-            HalfedgeIter h = v -> halfedge();
-            Vector3D original_neighbor_position_sum(0,0,0);
-            do {
-                original_neighbor_position_sum += h -> twin() -> vertex() -> position;
-                h = h -> twin() -> next();
-
-            } while (h != v -> halfedge());
-            // TODO: why float for degree
-            float n = (float) v -> degree();
-            // TODO: rewrite if condition
-            float u = (n == 3.0) ? (3.0 / 16.0) : (3.0 / (8.0 * n));
-            
-            // Set newPostion field for old vertices
-            v -> newPosition = (1.0 - n * u) * v -> position + u * original_neighbor_position_sum;
-            
+      // Set newPosition of old vertices as weighted combination of surrounding old vertices
+      HalfedgeIter h = v -> halfedge();
+      Vector3D original_neighbor_position_sum(0,0,0);
+      while (1) {
+        original_neighbor_position_sum += h -> twin() -> vertex() -> position;
+        h = h -> twin() -> next();
+        if (h == v -> halfedge()) break;
+      }
+      float n = (float) v -> degree(), u;
+      if (n == 3.0) u = (3.0 / 16.0);
+      else  u = (3.0 / (8.0 * n));
+      
+      // Set newPostion field for old vertices
+      v -> newPosition = (1.0 - n * u) * v -> position + u * original_neighbor_position_sum;
+      // set isNew to false for all old vertices
+      v -> isNew = false;
     }
 
-    
-    // 3. Split every edge in the mesh, in any order. For future reference, we're also going to store some
-    // information about which subdivide edges come from splitting an edge in the original mesh, and which edges
-    // are new, by setting the flat Edge::isNew. Note that in this loop, we only want to iterate over edges of
-    // the original mesh---otherwise, we'll end up splitting edges that we just split (and the loop will never end!)
+    // Compute the updated vertex positions associated with edges, and store it in Edge::newPosition.
+    for (EdgeIter e = mesh.edgesBegin(); e != mesh.edgesEnd();e++) {
+      // Inner Halfedges
+      HalfedgeIter h0 = e -> halfedge();
+      HalfedgeIter h1 = h0 -> next();
+      HalfedgeIter h2 = h1 -> next();
+      HalfedgeIter h3 = h0 -> twin();
+      HalfedgeIter h4 = h3 -> next();
+      HalfedgeIter h5 = h4 -> next();
+
+      // Vertices
+      VertexIter a = h3 -> vertex();  // second vertex at opposite endpoint of previous edge
+      VertexIter b = h0 -> vertex();  // first vertex at endpoint of previous edge
+      VertexIter d = h5 -> vertex();  // second vertex at opposite endpoint of flipped edge
+      VertexIter c = h2 -> vertex();  // first vertex at endpoint of flipped edge
+
+      // Precompute and store position of new vertices in newPosition field of old edges
+      e -> newPosition = 3.0 / 8.0 * (a -> position + b -> position) + 1.0 / 8.0 * (c -> position + d -> position);
+      // Set isNew to false for all old edges
+      e -> isNew = false;    
+    }
+
+    // Step B: Subdivide the original mesh via edge splits and flips as described.
+    // Split every edge in the mesh, in any order.
     for (EdgeIter e = mesh.edgesBegin(); e != mesh.edgesEnd(); e++) {
-            VertexIter v1 = e -> halfedge() -> vertex();
-            VertexIter v2 = e -> halfedge() -> twin() -> vertex();
+      VertexIter v1 = e -> halfedge() -> vertex(), v2 = e -> halfedge() -> twin() -> vertex();
 
-            // Split only if its an old edge (prevents infinite loop)
-            if(!v1 -> isNew && !v2 -> isNew) {
-                VertexIter v = mesh.splitEdge(e);
-                v -> newPosition = e -> newPosition; // set new vertex position
-            }
+      // Split only if its an old edge (prevents infinite loop)
+      if (!v1 -> isNew && !v2 -> isNew) mesh.splitEdge(e) -> newPosition = e -> newPosition; // set new vertex position
     }
     
-    // 4. Flip any new edge that connects an old and new vertex.
+    // Flip any new edge that connects an old and new vertex.
     for (EdgeIter e = mesh.edgesBegin(); e != mesh.edgesEnd(); e++) {
-            VertexIter v1 = e->halfedge()->vertex();
-            VertexIter v2 = e->halfedge()->twin()->vertex();
-
-            if(e->isNew && ((v1->isNew && !v2->isNew) || (!v1->isNew && v2->isNew))) {
-                mesh.flipEdge(e);  // automatically updates newPosition field in new vertices
-            }
+      VertexIter v1 = e -> halfedge() -> vertex(), v2 = e -> halfedge() -> twin() -> vertex();
+      if (e -> isNew && ((v1 -> isNew && !v2 -> isNew) || (!v1 -> isNew && v2 -> isNew))) mesh.flipEdge(e);  // automatically updates newPosition field in new vertices
     }
 
-    // 5. Copy the new vertex positions into final Vertex::position.
+    // Step C: Update all vertex positions in the subdivided mesh using the values already computed.
     for (VertexIter v = mesh.verticesBegin(); v != mesh.verticesEnd(); v++) {
-            v->position = v->newPosition;
-            v->isNew = false;
+      v -> position = v -> newPosition;
+      v -> isNew = false;
     } 
 
   }
